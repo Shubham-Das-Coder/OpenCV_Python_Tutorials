34becd3a38849cfad665f050dad3ab69db6edc78
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-14 23:03:49 +0530

    Add files via upload

---
+++ Images_5.py
@@ -0,0 +1,7 @@
+import cv2
+
+img=cv2.imread('E:\College Memories\WhatsApp Image 2021-12-30 at 11.23.13 PM (1).jpeg',1)
+img=cv2.resize(img,(400,400))
+cv2.imshow('Image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
